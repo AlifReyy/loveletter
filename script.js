0e79a11c26bc770e20d76f4039f3deceea6e563a
AlifReyy <alifreyfan26@gmail.com>
2024-10-27 20:03:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function acceptLove() {
+    document.getElementById("response").textContent = "Aku sangat senang! Terima kasih sudah menerima perasaanku ❤️";
+  }
+  
+  function rejectLove() {
+    document.getElementById("response").textContent = "Tidak apa-apa, aku tetap menghargaimu dan berharap kamu bahagia 😊";
+  }
+  
